80b38afde4fb96c895f830883c7514cbc1f6745f
Nathan Verjinski <nverjins@gmu.edu>
2014-12-01 16:29:34 -0500

    limits.h

---
+++ limits.h
@@ -0,0 +1,17 @@
+/* 	File originally from OS-161. This file has been largely edited and 
+	implemented by Nathan Verjinski and Kunal Sarkhel 		*/
+
+
+#ifndef _KERN_LIMITS_H_
+#define _KERN_LIMITS_H_
+
+/* Longest filename (without directory) not including null terminator */
+#define NAME_MAX  255
+
+/* Longest full path name */
+#define PATH_MAX   1024
+
+#define PID_MAX	512
+
+
+#endif /* _KERN_LIMITS_H_ */
